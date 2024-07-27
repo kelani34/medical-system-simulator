4340b7d1218fae0aeabbaede399a54644a88b212
kelani34 <kelanitaiwo34@gmail.com>
2024-07-27 15:59:01 +0100

    init

---
+++ src/MedicalDeviceControlSystem.java
@@ -0,0 +1,13 @@
+
+
+
+// Main Application Class
+public class MedicalDeviceControlSystem {
+    public static void main(String[] args) {
+        try {
+            int[] sensor = {93, 3,2, };
+        } catch (ArithmeticException e) {
+            // Error handling
+        }
+    }
+}
