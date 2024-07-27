45b1f4f42673e814d486320ebdca6e4db516893a
kelani34 <kelanitaiwo34@gmail.com>
2024-07-27 16:00:33 +0100

    add: sensor data class

--- src/MedicalDeviceControlSystem.java
+++ src/MedicalDeviceControlSystem.java
@@ -1,5 +1,31 @@
+import java.util.Random;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 
+class SensorData {
+    private static final Random random = new Random();
+    private static final Logger logger = Logger.getLogger(SensorData.class.getName());
+
+    public static double readHeartRate()  {
+        return readSensorData("Heart Rate");
+    }
+
+    public static double readBloodPressure()  {
+        return readSensorData("Blood Pressure");
+    }
+
+    public static double readTemperature()  {
+        return readSensorData("Temperature");
+    }
+
+    private static double readSensorData(String sensorName) {
+        if (random.nextInt(100) < 10) { // 10% chance of sensor failure
+            logger.log(Level.SEVERE, sensorName + " sensor malfunction.");
+        }
+        return 50 + (random.nextDouble() * 100); // random sensor data
+    }
+}
 
 // Main Application Class
 public class MedicalDeviceControlSystem {
